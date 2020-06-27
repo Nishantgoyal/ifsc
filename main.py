1d845105bb1c5d8639f65ade31f8f93751598c5d
Nishant Goyal <nishantgoyal@C02Z71ZQL410.local>
2020-06-27 13:07:49 +0530

    Removed extra code

--- main.py
+++ main.py
@@ -1,14 +1,1 @@
 from app import app
-
-
-# from flask import Flask
-# app = Flask(__name__)
-
-
-# @app.route("/")
-# def hello():
-#     return "hello World!"
-
-
-# flask run -h 0.0.0.0 -p 80
-# uwsgi --ini uwsgi.ini
